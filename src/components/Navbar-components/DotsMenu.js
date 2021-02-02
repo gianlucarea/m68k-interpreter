da42ad85b12d26758bb241ddeedaa9ca97d65fbb
Gianluca Rea <gianlucarea@localhost.localdomain>
2021-02-02 18:54:32 +0100

    guide 50%
    Committer: GianlucaRea <reagianluca97@gmail.com>

--- src/components/Navbar-components/DotsMenu.js
+++ src/components/Navbar-components/DotsMenu.js
@@ -1,12 +1,16 @@
 import React , {Component} from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 
 class DotsMenu extends Component {
     render(){
         return ( 
-            
-        <a href="#" id="navbar-dots"><FontAwesomeIcon icon={faEllipsisH} /></a>)  
+            <div>
+            <a href="/help.html" id="navbar-guide"><FontAwesomeIcon icon={faQuestionCircle} /></a>
+            <a href="#" id="navbar-dots"><FontAwesomeIcon icon={faEllipsisH} /></a>
+            </div>
+       )  
         }
     }
 
